d43c86f8ba361c84872c3fd4e976b16efbaae8c7
Aniket <thakur02aniket@gmail.com>
2024-02-15 10:01:47 +0530

    First Commit

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./NavBar";
+import Hero from "./Hero";
+import Billing  from "./Billing";
+import Business from "./Business";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+
+export {
+    Navbar, 
+    Hero, 
+    Business, 
+    CardDeal, 
+    Clients,
+    CTA, 
+    Stats, 
+    Footer, 
+    Testimonials,
+    Billing
+};
